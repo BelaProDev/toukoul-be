b0b414b12b45407bea29a25d6aa87bf8480b04be
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2026-01-10 21:02:41 +0000

    paragraph overflow

--- tools.js
+++ tools.js
@@ -1,22 +1,23 @@
 let moreLess = Array.from(document.querySelectorAll('.moreLess'))
-let bm = document.createElement('button')
-let bl = document.createElement('button')
-bm.textContent = "Voir plus"
-bl.textContent = "Voir moins"
-bm.addEventListener('click', function (e) {
-    e.target.previousElementSibling.style.overflow = ''
-    e.target.previousElementSibling.style.height = ''
-    e.target.after(bl)
-    e.target.remove()
-})
-bl.addEventListener('click', function (e) {
-    e.target.previousElementSibling.style.overflow = 'hidden'
-    e.target.previousElementSibling.style.height = '8.5em'
-    e.target.after(bm)
-    e.target.remove()
-})
+
 moreLess.forEach((e) => {
     if (e.innerText.length > 100) {
+        let bm = document.createElement('button')
+        let bl = document.createElement('button')
+        bm.textContent = "Voir plus"
+        bl.textContent = "Voir moins"
+        bm.addEventListener('click', function (e) {
+            e.target.previousElementSibling.style.overflow = ''
+            e.target.previousElementSibling.style.height = ''
+            e.target.after(bl)
+            e.target.remove()
+        })
+        bl.addEventListener('click', function (e) {
+            e.target.previousElementSibling.style.overflow = 'hidden'
+            e.target.previousElementSibling.style.height = '8.5em'
+            e.target.after(bm)
+            e.target.remove()
+        })
         e.style.overflow = 'hidden'
         e.style.height = '8.5em'
         e.after(bm)
